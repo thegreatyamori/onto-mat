46b37daecec8251f28c13b9cc8da175820c5e3aa
thegreatyamori <jersonmorocho16@gmail.com>
2020-09-08 00:44:15 -0500

    Updates

---
+++ precache-manifest.a594e191b1a83821b36d2998713a9967.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9eb9b0513699a91883e9f4f7b2a7c66",
+    "url": "/onto-mat/index.html"
+  },
+  {
+    "revision": "9346ad5e376bca5ff8b5",
+    "url": "/onto-mat/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "1d8877de01437a6ae234",
+    "url": "/onto-mat/static/css/main.b9aa6104.chunk.css"
+  },
+  {
+    "revision": "9346ad5e376bca5ff8b5",
+    "url": "/onto-mat/static/js/2.61da17da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/onto-mat/static/js/2.61da17da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8877de01437a6ae234",
+    "url": "/onto-mat/static/js/main.c92db6f5.chunk.js"
+  },
+  {
+    "revision": "05de76b0fe6f3c535947",
+    "url": "/onto-mat/static/js/runtime-main.9e5d1812.js"
+  }
+]);
